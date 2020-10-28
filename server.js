0a498902219da8a7784860e2d2146a9a8fc72186
Younghu Park <younghu@Younghus-MBP-2.attlocal.net>
2020-10-28 10:03:04 -0700

    added moveable object

--- server.js
+++ server.js
@@ -17,13 +17,44 @@ server.listen(5000, function () {
     console.log('Starting server on port 5000');
 });
 
-// Add the WebSocket handlers
+var players = {};
 io.on('connection', function (socket) {
+    socket.on('new player', function () {
+        players[socket.id] = {
+            x: 300,
+            y: 300
+        };
+    });
+    socket.on('movement', function (data) {
+        var player = players[socket.id] || {};
+        if (data.left) {
+            player.x -= 5;
+        }
+        if (data.up) {
+            player.y -= 5;
+        }
+        if (data.right) {
+            player.x += 5;
+        }
+        if (data.down) {
+            player.y += 5;
+        }
+    });
+    // socket.on('mousemove', function (data) {
+    //     console.log("yay");
+    //     var player = players[socket.id] || {};
+    //     if (data.x != 0) {
+    //         player.x += data.x;
+    //     }
+    //     if (data.y != 0) {
+    //         player.y += data.y;
+    //     }
+    // });
+
 });
 
 setInterval(function () {
-    io.sockets.emit('message', 'hi!');
-}, 1000);
+    io.sockets.emit('state', players);
 
-// hi
+}, 1000 / 60);
 
