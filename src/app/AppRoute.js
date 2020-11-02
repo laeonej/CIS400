1704a4a2fede04a54ecd002d48d790c84a22f7d6
Younghu Park <younghu@Younghus-MBP-2.attlocal.net>
2020-11-01 17:32:48 -0800

    added seperate files

--- src/app/AppRoute.js
+++ src/app/AppRoute.js
@@ -3,6 +3,9 @@ import React from "react";
 import { Menu } from './pages/Menu/MenuPage.js';
 import { Login } from './pages/Login/LoginPage.js';
 import { Temp } from './Temp.js';
+import { Game } from './Game.js';
+import { Card } from './Card.js';
+// import { Lobby } from './Lobby.js'
 import {
   BrowserRouter as Router,
   Route,
@@ -11,9 +14,9 @@ import {
 
 
 export default function AppRoute() {
-    return (
-      <Router>
-        {/* <div>
+  return (
+    <Router>
+      {/* <div>
           <nav>
             <ul>
               <li>
@@ -27,13 +30,16 @@ export default function AppRoute() {
               </li>
             </ul>
           </nav> */}
-          <Switch>
-            <Route component={Login} exact path='/login'/>
-            <Route component={Menu} exact path='/menu'/>
-            <Route component={Temp} exact path='/'/>
-  
-          </Switch>
-        {/* </div> */}
-      </Router>
-    );
-  }
+      <Switch>
+        <Route component={Login} exact path='/login' />
+        <Route component={Menu} exact path='/menu' />
+        {/* <Route component={Lobby} exact path='/lobby' /> */}
+        <Route component={Game} exact path='/game' />
+        <Route component={Card} exact path='/card' />
+        <Route component={Temp} exact path='/' />
+
+      </Switch>
+      {/* </div> */}
+    </Router>
+  );
+}
