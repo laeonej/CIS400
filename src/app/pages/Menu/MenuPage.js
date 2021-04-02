4b81c0253cab05d0f44dedd52a2cd45fcc1ea737
Jeffligit <54647089+Jeffligit@users.noreply.github.com>
2021-04-02 16:05:28 -0400

    added back herokuapp to endpoint

--- src/app/pages/Menu/MenuPage.js
+++ src/app/pages/Menu/MenuPage.js
@@ -14,7 +14,7 @@ export default function Menu(props) {
     const [menu, setMenu] = useState(true)
     const [createPage, setCreatePage] = useState(false)
     const [joinPage, setJoinPage] = useState(false)
-    const [endPoint, setEndPoint] = useState("http://localhost:5000")
+    const [endPoint, setEndPoint] = useState("https://apricot-shortcake-33947.herokuapp.com/")
     const [thisSocket, setThisSocket] = useState(null)
     const [inGame, setInGame] = useState(false)
     const [tableCode, setTableCode] = useState('AAAA11')
@@ -63,7 +63,7 @@ export default function Menu(props) {
             //updateAnalytics({ "type": "numDisconnections" })
 
         }
-    }, [setTableCode, tableCode])
+    }, [tableCode])
 
 
     function createGame(enteredPlayerName) {
